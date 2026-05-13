ac3b40af6dd273b08bcfb45f16fdc3d2d304e2e6
richard254120 <ogudarichard254@gmail.com>
2026-05-13 08:37:15 +0300

    Fix backend URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://axx-spaces-backend.onrender.com/api",
+    "https://axx-spaces-backend-1.onrender.com/api",
 });
-
 export default API;
