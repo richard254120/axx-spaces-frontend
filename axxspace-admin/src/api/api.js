dfdb50d287f7b0ac3e7d871416de5a9311527b3d
richard254120 <ogudarichard254@gmail.com>
2026-05-28 07:23:20 +0300

    Production build update

--- axxspace-admin/src/api/api.js
+++ axxspace-admin/src/api/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://axx-spaces-backend-1.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://axx-spaces-backend-1.onrender.com/api",
 });
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
 export default API;
