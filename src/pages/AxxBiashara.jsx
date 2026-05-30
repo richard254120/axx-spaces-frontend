adf123b534b22026e3487844c8b3dbf63558781a
richard254120 <ogudarichard254@gmail.com>
2026-05-31 00:09:53 +0300

    Fix bugs

--- src/pages/AxxBiashara.jsx
+++ src/pages/AxxBiashara.jsx
@@ -402,6 +402,16 @@ const BUSINESS_CATEGORIES = [
   "Other",
 ];
 
+const KENYA_COUNTIES = [
+  "Mombasa", "Kwale", "Kilifi", "Tana River", "Lamu", "Taita Taveta",
+  "Garissa", "Wajir", "Mandera", "Marsabit", "Isiolo", "Meru", "Tharaka Nithi",
+  "Embu", "Kitui", "Machakos", "Makueni", "Nyandarua", "Nyeri", "Kirinyaga",
+  "Murang'a", "Kiambu", "Turkana", "West Pokot", "Samburu", "Trans Nzoia",
+  "Uasin Gishu", "Elgeyo Marakwet", "Nandi", "Baringo", "Laikipia", "Nakuru",
+  "Narok", "Kajiado", "Kericho", "Bomet", "Kakamega", "Vihiga", "Bungoma",
+  "Busia", "Siaya", "Kisumu", "Homa Bay", "Migori", "Kisii", "Nyamira", "Nairobi City",
+];
+
 const BADGE_CONFIG = {
   student_verified: { label: "🟢 Student Verified", style: styles.badgeStudent },
   identity_verified: { label: "🟢 Identity Verified", style: styles.badgeIdentity },
