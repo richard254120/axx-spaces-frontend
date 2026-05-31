fab6e621b4e98eb4aba301cd6f3557a5cc2dc066
richard254120 <ogudarichard254@gmail.com>
2026-05-31 16:55:54 +0300

    Updated project

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import ReactGA from 'react-ga4'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </BrowserRouter>
 )
