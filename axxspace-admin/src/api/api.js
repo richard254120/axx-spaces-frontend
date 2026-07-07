b7d85797f8c84c8f0498aa5b1216a51688a44712
richard254120 <ogudarichard254@gmail.com>
2026-07-07 08:22:34 +0300

    Your commit message

--- axxspace-admin/src/api/api.js
+++ axxspace-admin/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:1000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:1001/api",
 });
 
 API.interceptors.request.use((config) => {
