3338055378fdc24a796c9df629c2acbd9673cef5
richard254120 <ogudarichard254@gmail.com>
2026-09-20 14:09:32 +0300

    update

--- axxspace-admin/src/api/api.js
+++ axxspace-admin/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:1001/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://axx-spaces-backend-1.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
